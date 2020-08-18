4b590eb05aff80cf29a11512aee43525e6ba7242
FarjanaFerdousiTamanna <farjana.ferdousi.tamanna@gmail.com>
2020-08-18 16:13:39 +0600

    Add project files.

---
+++ C_Sharp_SixthProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_Sharp_SixthProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] names = new string[4];  //here, creating array by the command, String[] names
+                                             // and allocating memory by new string [4];
+            names[0] = "Tamanna";
+            names[1] = "Mou";
+            names[2] = "Rakib";
+            names[3] = "Khan";
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+        }
+    }
+}
